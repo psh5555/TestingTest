273d722783999ce091db7b04d175dba861465812
psh5555 <terpis5479@gmail.com>
2021-05-20 10:04:20 +0900

    init commit

---
+++ src/test/java/TravisCITest/LargestTest.java
@@ -0,0 +1,25 @@
+package TravisCITest;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class LargestTest {
+
+	@Test
+	public void test1() {
+		assertEquals(9, Largest.largest(new int[] {7,8,9}));
+	}
+
+	
+	@Test
+	public void test2() {
+		assertEquals(9, Largest.largest(new int[] {9,8,7}));
+	}
+
+	
+	@Test
+	public void test3() {
+		assertEquals(20, Largest.largest(new int[] {8,20,7}));
+	}
+
+}
